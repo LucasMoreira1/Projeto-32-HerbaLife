16cceacb6c562d1246908be66b6f86707aeedd2b
Lucas Moreira <lucas_moreira@outlook.com>
2023-09-10 17:31:46 -0300

    script preco-total funcionando

--- script.js
+++ script.js
@@ -42,7 +42,12 @@ pdvBtn.addEventListener("click", () => {
 
             // Initialize Flowbite components after content is added
             initFlowbite(); // or initDropdowns(), initModals(), etc.
+            // Create a script element and append it to the content area to execute the script
+            const script = document.createElement("script");
+            script.textContent = "initPDVPage();"; // Call the initPDVPage function
+            contentArea.appendChild(script);
         });
+        
 });
 
 estoqueBtn.addEventListener("click", () => {
@@ -68,32 +73,112 @@ function toggleSidebar() {
     sidebar.classList.toggle("md:w-64"); // For desktop view
 }
 
-const quantityInput = document.getElementById("first_product");
-const priceCell = document.getElementById("first_product_price");
-const totalPriceCell = document.getElementById("first_product_total");
+function initPDVPage() {
+    totalFirstProd()
+    totalSecondProd()
+    totalThirdProd()  
+    
 
-// Function to calculate and update total price
-function updateTotalPrice() {
-    // Get the quantity value as a number
-    const quantity = parseFloat(quantityInput.value);
-
-    // Get the price value as a number (remove the "$" symbol and any non-numeric characters)
-    const price = parseFloat(priceCell.innerText.replace(/[^0-9.]/g, ''));
-
-    // Calculate the total price
-    const totalPrice = quantity * price;
-
-    console.log("Quantity:", quantity);
-    console.log("Price:", price);
-    console.log("Total Price:", totalPrice);
-
-    // Update the total price cell with the result
-    totalPriceCell.innerText = "$" + totalPrice.toFixed(2); // Format as currency with 2 decimal places
 }
 
-// Add an event listener to the quantity input to trigger the calculation when it changes
-quantityInput.addEventListener("input", updateTotalPrice);
+function totalFirstProd() {
+    const quantityInput = document.getElementById("first_product");
+    const priceCell = document.getElementById("first_product_price");
+    const totalPriceCell = document.getElementById("first_product_total");
 
-// Initial calculation (if needed)
-updateTotalPrice();
+    // Function to calculate and update total price
+    function updateTotalPrice() {
+        // Get the quantity value as a number
+        const quantity = parseFloat(quantityInput.value);
+        
+        // Get the price value as a number (remove any non-numeric characters, including "R$" and commas)
+        const price = parseFloat(priceCell.innerText.replace(',', '.').replace(/[^0-9.]/g, ''));
 
+        // Calculate the total price
+        const totalPrice = quantity * price;
+
+        console.log("Quantity:", quantity);
+        console.log("Price:", price);
+        console.log("Total Price:", totalPrice);
+
+        // Format the total price as "R$X,XX"
+        const formattedTotalPrice = "R$" + totalPrice.toFixed(2).replace('.', ',');
+
+        // Update the total price cell with the formatted result
+        totalPriceCell.innerText = formattedTotalPrice;
+    }
+
+    // Add an event listener to the quantity input to trigger the calculation when it changes
+    quantityInput.addEventListener("input", updateTotalPrice);
+
+    // Initial calculation (if needed)
+    updateTotalPrice();
+}
+
+function totalSecondProd() {
+    const quantityInput2 = document.getElementById("second_product");
+    const priceCell2 = document.getElementById("second_product_price");
+    const totalPriceCell2 = document.getElementById("second_product_total");
+
+    // Function to calculate and update total price
+    function updateTotalPrice2() {
+        // Get the quantity value as a number
+        const quantity2 = parseFloat(quantityInput2.value);
+        
+        // Get the price value as a number (remove any non-numeric characters, including "R$" and commas)
+        const price2 = parseFloat(priceCell2.innerText.replace(',', '.').replace(/[^0-9.]/g, ''));
+
+        // Calculate the total price
+        const totalPrice2 = quantity2 * price2;
+
+        console.log("Quantity:", quantity2);
+        console.log("Price:", price2);
+        console.log("Total Price:", totalPrice2);
+
+        // Format the total price as "R$X,XX"
+        const formattedTotalPrice2 = "R$" + totalPrice2.toFixed(2).replace('.', ',');
+
+        // Update the total price cell with the formatted result
+        totalPriceCell2.innerText = formattedTotalPrice2;
+    }
+
+    // Add an event listener to the quantity input to trigger the calculation when it changes
+    quantityInput2.addEventListener("input", updateTotalPrice2);
+
+    // Initial calculation (if needed)
+    updateTotalPrice2();
+}
+
+function totalThirdProd() {
+    const quantityInput3 = document.getElementById("third_product");
+    const priceCell3 = document.getElementById("third_product_price");
+    const totalPriceCell3 = document.getElementById("third_product_total");
+
+    // Function to calculate and update total price
+    function updateTotalPrice3() {
+        // Get the quantity value as a number
+        const quantity3 = parseFloat(quantityInput3.value);
+        
+        // Get the price value as a number (remove any non-numeric characters, including "R$" and commas)
+        const price3 = parseFloat(priceCell3.innerText.replace(',', '.').replace(/[^0-9.]/g, ''));
+
+        // Calculate the total price
+        const totalPrice3 = quantity3 * price3;
+
+        console.log("Quantity:", quantity3);
+        console.log("Price:", price3);
+        console.log("Total Price:", totalPrice3);
+
+        // Format the total price as "R$X,XX"
+        const formattedTotalPrice3 = "R$" + totalPrice3.toFixed(2).replace('.', ',');
+
+        // Update the total price cell with the formatted result
+        totalPriceCell3.innerText = formattedTotalPrice3;
+    }
+
+    // Add an event listener to the quantity input to trigger the calculation when it changes
+    quantityInput3.addEventListener("input", updateTotalPrice3);
+
+    // Initial calculation (if needed)
+    updateTotalPrice3();
+}
